c224ca810853c52e668e6b4b1e78d538646a21d6
Casey N Brown <cbrown89@gorf.ccs.neu.edu>
2010-11-03 15:35:56 -0400

    Fixed errors found in review

--- growler/views.py
+++
@@ -1,6 +0,0 @@
-from django.template import Context, loader
-from django.http import HttpResponse
-
-def index(request):
-    t = loader.get_template('growler/index.html')
-    return HttpResponse(t.render())
