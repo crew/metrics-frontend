7d0e4d60072ce545c97521978925f333f0ea6a78
Gary Katsevman <me@gkatsev.com>
2011-02-16 21:50:14 -0500

    some UI changes and functionality enhancements

--- growler/media/js/view.js
+++ growler/media/js/view.js
@@ -107,73 +107,82 @@ labMetrics.create_chart = function (data) {
 };
 
 labMetrics.reloadChart = function(start, end){
-  labMetrics.start.datetimepicker('setDate', start);
-  labMetrics.end.datetimepicker('setDate', end);
+  var stime
+    , etime
+    , ns = "ns";
 
-  $('#fetch').click();
+  debugger;
+
+  start && labMetrics.start.datetimepicker('setDate', start);
+  end && labMetrics.end.datetimepicker('setDate', end);
+
+  var now = new Date();
+
+  stime = labMetrics.start.datetimepicker('getDate');
+  etime = labMetrics.end.datetimepicker('getDate');
+
+  if(now - etime < 0) {
+    etime = now;
+    labMetrics.end.datetimepicker('setDate', now);
+  }
+  if(now - stime < 0) {
+    stime = now;
+    labMetrics.start.datetimepicker('setDate', now);
+  }
+  if(etime - stime < 0) {
+    alert('please make sure that the ending date is after the starting date');
+    return false;
+  }
+  else {
+
+    $('#fetch').html('<img src="/media/graphics/ajax-loader.gif">');
+
+    $.ajax({
+        type: "GET"
+      , url: labMetrics.jsonViewUrl
+      , success: labMetrics.create_chart
+      , complete: function(){ $('#fetch').html('Fetch'); }
+      , data: {
+            'ns': ns
+          , 'start': stime.getTime()/1000
+          , 'end': etime.getTime()/1000
+        }
+      });
+    }
 };
 
 labMetrics.run = function(jsonViewUrl){
-  var stime
-    , etime
-    , ns = "ns"
-    , last24 = labMetrics.getLast24()
+  var last24 = labMetrics.getLast24()
     , reload = $('#reload');
+
+  labMetrics.jsonViewUrl = jsonViewUrl;
 
   labMetrics.start = $('#start');
   labMetrics.end = $('#end');
 
-  labMetrics.start.datetimepicker();
-  labMetrics.end.datetimepicker();
+  labMetrics.start.datetimepicker({
+      onClose: function(){ $('#fetch').click(); }
+    });
+  labMetrics.end.datetimepicker({
+      onClose: function(){ $('#fetch').click(); }
+    });
 
   reload.click(function(e) {
     if(labMetrics.reload) {
       labMetrics.reload = false;
-      reload.html('Auto reload?');
+      reload.html('Auto Reloading OFF.');
     }
     else {
       labMetrics.reload = true;
-      reload.html('Reloading...');
+      reload.html('Auto Reloading ON.');
       loop();
     }
   });
 
   $("#fetch").click(function (e) {
-    var now = new Date();
-
-    $('#fetch').html('<img src="/media/graphics/ajax-loader.gif">');
-
-    e.preventDefault();
-
-    stime = labMetrics.start.datetimepicker('getDate');
-    etime = labMetrics.end.datetimepicker('getDate');
-
-    if(now - etime < 0) {
-      etime = now;
-      labMetrics.end.datetimepicker('setDate', now);
-    }
-    if(now - stime < 0) {
-      stime = now;
-      labMetrics.start.datetimepicker('setDate', now);
-    }
-    if(etime - stime < 0) {
-      alert('please make sure that the ending date is after the starting date');
-      return false;
-    }
-    else {
-
-      $.ajax({
-          type: "GET"
-        , url: jsonViewUrl
-        , success: labMetrics.create_chart
-        , complete: function(){ $('#fetch').html('Fetch'); }
-        , data: {
-              'ns': ns
-            , 'start': stime.getTime()/1000
-            , 'end': etime.getTime()/1000
-          }
-        });
-      }
+      e.preventDefault();
+      labMetrics.reload && reload.click();
+      labMetrics.reloadChart();
   });
 
 
