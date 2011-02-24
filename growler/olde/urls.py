eff49f57087ee4a85694ce79bb5c762e571a1879
Nick Aldwin <aldwin@ccs.neu.edu>
2011-02-24 10:16:41 -0500

    Add KML utility and KML urls

--- growler/olde/urls.py
+++ growler/olde/urls.py
@@ -8,6 +8,8 @@ urlpatterns = patterns('%s.olde.views' % settings.MODULE,
     url(r'^json/windows/data/$', 'json_windows_machines_data', name='json-windows-machines-data'),
     url(r'^json/view/all$', 'json_view_all', name='json-view-all'),
     url(r'^windows/$', 'windows', name='windows'),
+    url(r'^kml/windows/current/$', 'kml_windows_current', name='kml-windows-current'),
+    url(r'^kml/windows/hist/$', 'kml_windows_historical', name='kml-windows-historical'),
     url(r'^view/$', 'view', name='view'),
     url(r'^$', 'index', name='index'),
 )
